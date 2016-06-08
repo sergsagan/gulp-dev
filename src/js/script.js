9db2dbba23017501fce5d1056b02a81998e1b0d1
zorro76 <sergey.martynuk@gmail.com>
2016-06-08 10:55:14 +0300

    add gulp-rigger, form check

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,6 @@ $(function() {
 
     $("head").append("<link rel='stylesheet' type='text/css' href='css/vendor.css' />");
 
-    $("head").append("<link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,400italic' rel='stylesheet' type='text/css'>");
-
     $("head").append("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />");
 
     $('.fade').slick({
@@ -75,7 +73,72 @@ $(function() {
         return true;
     });
 
+
+    $('.rf').each(function(){
+        // Объявляем переменные (форма и кнопка отправки)
+        var form = $(this),
+            btn = form.find('.btn_submit');
+
+        // Добавляем каждому проверяемому полю, указание что поле пустое
+        form.find('.rfield').addClass('empty_field');
+
+        // Функция проверки полей формы
+        function checkInput(){
+            form.find('.rfield').each(function(){
+                if($(this).val() != ''){
+                    // Если поле не пустое удаляем класс-указание
+                    $(this).removeClass('empty_field');
+                } else {
+                    // Если поле пустое добавляем класс-указание
+                    $(this).addClass('empty_field');
+                }
+            });
+        }
+
+        // Функция подсветки незаполненных полей
+        function lightEmpty(){
+            form.find('.empty_field').css({'border-color':'#d8512d'});
+            // Через полсекунды удаляем подсветку
+            setTimeout(function(){
+                form.find('.empty_field').removeAttr('style');
+            },500);
+        }
+
+        // Проверка в режиме реального времени
+        setInterval(function(){
+            // Запускаем функцию проверки полей на заполненность
+            checkInput();
+            // Считаем к-во незаполненных полей
+            var sizeEmpty = form.find('.empty_field').size();
+            // Вешаем условие-тригер на кнопку отправки формы
+            if(sizeEmpty > 0){
+                if(btn.hasClass('disabled')){
+                    return false
+                } else {
+                    btn.addClass('disabled')
+                }
+            } else {
+                btn.removeClass('disabled')
+            }
+        },500);
+
+        // Событие клика по кнопке отправить
+        btn.click(function(){
+            if($(this).hasClass('disabled')){
+                // подсвечиваем незаполненные поля и форму не отправляем, если есть незаполненные поля
+                lightEmpty();
+                return false
+            } else {
+                // Все хорошо, все заполнено, отправляем форму
+                form.submit();
+            }
+        });
+    });
+
     $("#phone").mask("+38 (999) 999-99-99");
+
+
+
 
     //Аякс отправка форм
     //Документация: http://api.jquery.com/jquery.ajax/
