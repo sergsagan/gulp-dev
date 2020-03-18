5d0e4347d56d67e4247c28affb23293020c0db38
sergiy <sergey.martynuk@gmail.com>
2020-03-18 23:36:58 +0200

    add slider && section smm

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,14 @@ $(function() {
 	
     $("head").append("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />");
 
+    $('.slider').slick({
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
 });
