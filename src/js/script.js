669db39486b45ec5eee3396d98a4d413ddbdf61d
sergiy <sergey.martynuk@gmail.com>
2020-03-24 13:06:17 +0200

    add responsive part4

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ $(function() {
 	
     $("head").append("<link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' />");
 
+    
+
     $('.slider').slick({
         infinite: true,
         speed: 500,
